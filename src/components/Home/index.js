3fee2c1f1b5baef60b3a0995ca40ce449fa76971
bruno <brunomestres@gmail.com>
2021-07-27 15:46:20 -0300

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello world</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
